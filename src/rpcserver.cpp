eons-finance/ALQO

broke up the RPC commands in create/decode/relay

src/rpcserver.cpp
@@ -356,7 +356,9 @@ static const CRPCCommand vRPCCommands[] =
         {"pivx", "listmasternodes", &listmasternodes, true, true, false},
         {"pivx", "getmasternodecount", &getmasternodecount, true, true, false},
         {"pivx", "masternodeconnect", &masternodeconnect, true, true, false},
-		{"pivx", "masternodebroadcast", &masternodebroadcast, true, true, false},
+		{"pivx", "createmasternodebroadcast", &createmasternodebroadcast, true, true, false},
+		{"pivx", "decodemasternodebroadcast", &decodemasternodebroadcast, true, true, false},
+		{"pivx", "relaymasternodebroadcast", &relaymasternodebroadcast, true, true, false},
         {"pivx", "masternodecurrent", &masternodecurrent, true, true, false},
         {"pivx", "masternodedebug", &masternodedebug, true, true, false},
         {"pivx", "startmasternode", &startmasternode, true, true, false},

sriharikapu/ion

src/rpcserver.cpp
@@ -357,7 +357,9 @@ static const CRPCCommand vRPCCommands[] =
         {"ion", "listmasternodes", &listmasternodes, true, true, false},
         {"ion", "getmasternodecount", &getmasternodecount, true, true, false},
         {"ion", "masternodeconnect", &masternodeconnect, true, true, false},
-        {"ion", "masternodebroadcast", &masternodebroadcast, true, true, false},
+        {"ion", "createmasternodebroadcast", &createmasternodebroadcast, true, true, false},
+        {"ion", "decodemasternodebroadcast", &decodemasternodebroadcast, true, true, false},
+        {"ion", "relaymasternodebroadcast", &relaymasternodebroadcast, true, true, false},
         {"ion", "masternodecurrent", &masternodecurrent, true, true, false},
         {"ion", "masternodedebug", &masternodedebug, true, true, false},
         {"ion", "startmasternode", &startmasternode, true, true, false},