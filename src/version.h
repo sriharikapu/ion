779dad20ec7f5e55853200f9de275154e9fee1c2
FornaxA <fornaxa@servitising.org>
2018-07-16 09:07:40 +0200

    Bump minimum protocol version after upgrade enforcement

--- src/version.h
+++ src/version.h
@@ -22,7 +22,7 @@ static const int GETHEADERS_VERSION = 95610;
 
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 95610;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 95700;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 95701;
 
 //! masternodes older than this proto version use old strMessage format for mnannounce
 static const int MIN_PEER_MNANNOUNCE = 95701;
